a2957d3436cb7999f829253fbc703252c2379804
justheuristic <justheuristic@gmail.com>
2021-09-08 10:14:29 +0300

    Update noop.py

--- lib/training/noop.py
+++ lib/training/noop.py
@@ -31,19 +31,24 @@ class NoOpScheduler(LRSchedulerBase):
 
 class IgnoreGradManipulations(nn.Module):
     """ Wrapper for model that blocks gradient manipulations in huggingface Trainer (e.g. zero_grad, clip_grad) """
-    def __init__(self, module):
+    def __init__(self, module, override_clipping: bool = True, override_zero_grad: bool = True):
         super().__init__()
         self.module = module
+        self.override_clipping = override_clipping
+        self.override_zero_grad = override_zero_grad
 
     def forward(self, *args, **kwargs):
         return self.module.forward(*args, **kwargs)
 
     def zero_grad(self, set_to_none: bool = False) -> None:
-        if all(param.grad.isfinite().all() for param in self.parameters()):
+        if self.override_zero_grad and all(param.grad.isfinite().all() for param in self.parameters()):
             logger.debug("Successfully bypassed zero_grad")
         else:
-            self.module.zero_grad()
+            self.module.zero_grad(set_to_none=set_to_none)
 
-    def clip_grad_norm_(self, *args, **kwargs):
+    def clip_grad_norm_(self, max_norm: float, norm_type: int = 2):
         """ ignore clip_grad_norm on each step, clip in optimizer instead """
-        logger.debug("Successfully bypassed clip_grad_norm_")
+        if self.override_clipping:
+            logger.debug("Successfully bypassed clip_grad_norm_")
+        else:
+            return torch.nn.utils.clip_grad_norm_(self.module.parameters(), max_norm, norm_type=norm_type)
